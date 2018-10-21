5920b708d7491544248ff10bf985ea57464fd637
werkaska3 <weronika3@o2.pl>
2018-10-21 12:56:44 +0200

    aktualizacja

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 package com.sda.kolekcje;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<String>;
     }
 }
